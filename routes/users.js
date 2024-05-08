830afbcc72053a403d61d05e8d330e5eaed91a6a
PanutaVadim <149516814+PanutaVadim@users.noreply.github.com>
2024-05-08 20:56:27 +0300

    Подключение остальных коллекций

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
+const sendAllUsers = require('../controllers/users');
+const findAllUsers = require('../middlewares/users');
+
 const usersRouter = require('express').Router();
 
-usersRouter.get("/users", (req, res) => {})
+usersRouter.get("/users", findAllUsers, sendAllUsers)
 
 module.exports = usersRouter;
 
