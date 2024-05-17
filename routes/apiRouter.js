08c070421df17c586d70d6d7c651f78896cc4f9e
PanutaVadim <149516814+PanutaVadim@users.noreply.github.com>
2024-05-17 13:42:20 +0300

    Авторизация в процессе

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -3,7 +3,7 @@ const usersRouter = require("./users");
 const categoriesRouter = require("./categories");
 
 const apiRouter = require("express").Router();
-
+ 
 apiRouter.use("/api", gamesRouter);
 apiRouter.use("/api", usersRouter);
 apiRouter.use("/api", categoriesRouter);
